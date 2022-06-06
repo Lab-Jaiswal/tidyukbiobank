2718758ca5ff1abcdcade883dd87fee94dcb5b07
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-06-06 11:59:24 -0700

    Update get_genotypes.R

--- R/get_genotypes.R
+++ R/get_genotypes.R
@@ -2,14 +2,14 @@
 #' Function output: dataframe containing eid column and columns with the genotype information for the variants of interest
 #' 
 #' @variants list of the variants of interest
-#' @variant_df df containing all of the variants (created via get_directory_variants)
+#' @variants_df df containing all of the variants (created via get_directory_variants)
 #' @param directory directory containing the psams, pgens, and pvars
 #' @keywords get genotypes
 #' @export
 #' @examples
 #' get_genotypes()
 
-get_genotypes <- function(variants, variant_df, directory){
+get_genotypes <- function(variants, variants_df, directory){
   variants_df_filter <- filter(variants_df, is_in(variant_id, variants))
   chrom_list <- pull(variants_df_filter, chromosome) %>% unique()
   pgen_list <- get_pgen_list(chrom_list, directory)
