b6990c78731213de3d65e3334cbecbb6cba00ced
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2023-08-10 10:58:14 -0700

    Update get_self_reported_table.R

--- R/get_self_reported_table.R
+++ R/get_self_reported_table.R
@@ -4,16 +4,25 @@
 #' 
 #' @param diagnosis a number code or string describing the self reported disease of interest
 #' @param ukb_data the originial phenotype dataframe containing all individuals in the ukbiobank (~500,000 cols x 18,000 rows as of 09/07/2021)
+#' @param cancer
 #' @keywords get self reported table
 #' @export
 #' @examples
 #' get_self_reported_table()
 
-get_self_reported_table <- function(diagnosis, ukb_data){
-  coding <- parse_get_SR_table_input(diagnosis) 
-  self_report <- select(ukb_data, eid, contains("noncancer_illness_code_selfreported"))
-  diagnosis_long <- pivot_longer(self_report, -eid, names_to = "Diagnosis_Column", values_to = "diagnosis")
-  eid_positive_list <- filter(diagnosis_long, diagnosis == coding) %>% pull("eid") %>% unique() 
+get_self_reported_table <- function(diagnosis, ukb_data, cancer){
+  coding <- parse_get_SR_table_input(diagnosis, cancer) 
+
+  if (cancer == TRUE){
+    self_report <- select(ukb_data, eid, contains("cancer_code_selfreported"))
+    diagnosis_long <- pivot_longer(self_report, -eid, names_to = "Diagnosis_Column", values_to = "diagnosis") %>% filter(!is.na(diagnosis))
+    eid_positive_list <- filter(diagnosis_long, diagnosis == coding) %>% pull("eid") %>% unique()
+  } else {
+    self_report <- select(ukb_data, eid, contains("noncancer_illness_code_selfreported"))
+    diagnosis_long <- pivot_longer(self_report, -eid, names_to = "Diagnosis_Column", values_to = "diagnosis")
+    eid_positive_list <- filter(diagnosis_long, diagnosis == coding) %>% pull("eid") %>% unique() 
+  }
+
   with_SR <- filter(ukb_data, is_in(eid, eid_positive_list)) %>% select(eid)
   with_SR$self_reported_col <- 1
   without_SR <- filter(ukb_data, !is_in(eid, eid_positive_list)) %>% select(eid)
