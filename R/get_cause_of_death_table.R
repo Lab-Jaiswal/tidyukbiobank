f9abc59683419b4e7568e7b6a82531ca48aa7a5a
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2023-08-16 12:25:59 -0700

    Update get_cause_of_death_table.R

--- R/get_cause_of_death_table.R
+++ R/get_cause_of_death_table.R
@@ -10,9 +10,13 @@
 
 get_cause_of_death_table <- function(description, ukb_data){
   COD_eids <- get_cause_of_death_eids(description, ukb_data)
+  COD_description <- get_cause_of_death_description(description, ukb_data)
+
   COD_df <- select(ukb_data, eid) %>% mutate(hx = case_when(eid %in% COD_eids ~ 1, !eid %in% COD_eids ~ 0)) 
   
   COD_diagnosis <- str_c("Cause_of_Death_Included_", description, "_Description")
   colnames(COD_df) <- c("eid", COD_diagnosis) 
-  COD_df     
+
+  COD_df_combined <- left_join(COD_df, COD_description)
+  COD_df_combined     
 }
