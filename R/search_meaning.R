40b2f4f2c119b7534ec9abbd91721facafb06626
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2023-08-16 11:50:33 -0700

    Update search_meaning.R

--- R/search_meaning.R
+++ R/search_meaning.R
@@ -10,14 +10,14 @@
 
 search_meaning <- function(element, cancer){
     if (cancer != "TRUE"){
-        self_reported_df_loc <- system.file("extdata", "self_reported_df", package = "tidyUkBioBank")
+        self_reported_df_loc <- system.file("extdata", "coding6.rds", package = "tidyUkBioBank")
         self_reported_df <- read_rds(self_reported_df_loc)
         element <- tolower(element)
         relevant_meaning <- filter(self_reported_df, str_detect(meaning, element))
         relevant_meaning
     } else {
-        self_reported_df_loc <- system.file("extdata", "self_reported_cancer", package = "tidyUkBioBank")
-        self_reported_df <- read_tsv(self_reported_df_loc)
+        self_reported_df_loc <- system.file("extdata", "coding3.rds", package = "tidyUkBioBank")
+        self_reported_df <- read_rds(self_reported_df_loc)
         element <- tolower(element)
         relevant_meaning <- filter(self_reported_df, str_detect(meaning, element))
         relevant_meaning
