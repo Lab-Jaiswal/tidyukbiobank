1840e095aa029e0312af53319ecce85b84513d9b
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2023-08-17 13:58:09 -0700

    Update parse_get_SR_table_input.R

--- R/parse_get_SR_table_input.R
+++ R/parse_get_SR_table_input.R
@@ -17,7 +17,7 @@ parse_get_SR_table_input <- function(disease, cancer) {
         name <- SR_tibble %>% select(meaning)
         print(paste("You have selected the following self reported disease:", name, "(code:", coding, ")"))
     } else {
-        print("You have not inputted a valid string. All strings must have no spaces. Please see coding6 and coding3 for appropriate entries")
+        print("You have not inputted a valid string for either the 'disease' field (in parse_get_SR_table_input, self_reported_table, or self_reported_counts) or the 'self_reported` field (in diagnoses_table or diagnoses_counts). Please see coding6 and coding3 for appropriate entries.")
     }
   } else { 
     SR_tibble <- search_coding(disease, cancer)    
@@ -26,7 +26,7 @@ parse_get_SR_table_input <- function(disease, cancer) {
         name <- SR_tibble %>% select(meaning)
         print(paste("You have selected the following self reported disease:", name, "(code:", coding, ")"))
         } else {
-        print("You have not inputted a valid string. All strings must have no spaces. Please see coding6 and coding3 for appropriate entries.")
+        print("You have not inputted a valid code for either the 'disease' field (in parse_get_SR_table_input, self_reported_table, or self_reported_counts) or the 'self_reported` field (in diagnoses_table or diagnoses_counts). Please see coding6 and coding3 for appropriate entries.")
     }
   }
     coding
