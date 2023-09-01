1ec2a7cecaaa8534d9953b66e8dac4a42c1fbe48
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2023-09-01 12:45:42 -0700

    Update search_coding.R

--- R/search_coding.R
+++ R/search_coding.R
@@ -11,13 +11,13 @@
 
 search_coding <- function(element, cancer){
   if (cancer != TRUE) {
-    self_reported_df_loc <- system.file("extdata", "coding6.rds", package = "tidyUkBioBank")
+    self_reported_df_loc <- system.file("extdata", "coding6.rds", package = "tidyukbiobank")
     self_reported_df <- read_rds(self_reported_df_loc)
     element <- tolower(element)
     relevant_code <- filter(self_reported_df, str_detect(coding, element))
     relevant_code
   } else {
-    self_reported_df_loc <- system.file("extdata", "coding3.rds", package = "tidyUkBioBank")
+    self_reported_df_loc <- system.file("extdata", "coding3.rds", package = "tidyukbiobank")
     self_reported_df <- read_rds(self_reported_df_loc)
     element <- tolower(element)
     relevant_code <- filter(self_reported_df, str_detect(coding, element))
